45d2c609d3d16bf7b89e23644b6773cddc4036cc
danite <luansnain@gmail.com>
2021-12-07 23:18:11 +0100

    Add some logs to test the key pressed event

--- src/application.rs
+++ src/application.rs
@@ -1,4 +1,4 @@
-use nain_events::{EventCategory, KeyPressedEvent};
+use nain_events::{Event, EventCategory, KeyPressedEvent};
 use nain_log as log;
 
 pub trait Application {
@@ -6,7 +6,13 @@ pub trait Application {
         println!("Welcome to the Nain engine!");
 
         let event = KeyPressedEvent::new(2, 5);
+
         log::trace!("{}", event);
+        log::trace!("{}", event.get_name());
+
+        if event.is_in_category(EventCategory::Keyboard) {
+            log::trace!("Event category: {}", stringify!(EventCategory::Keyboard));
+        }
 
         loop {}
     }
